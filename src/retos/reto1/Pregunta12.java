0445e56f32db4849005289c2373dcf61f03edd21
Eduardo Flores <lalofloresn5@gmail.com>
2025-10-10 10:01:18 -0600

    Reto1 p12 y 13
    Se actualizó hasta retos y se agregaron las clases de la pregunta 12 y13

--- src/retos/reto1/Pregunta12.java
+++ src/retos/reto1/Pregunta12.java
@@ -1,14 +1,26 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package retos.reto1;
-
+import java.util.Scanner;
 /**
- *
+ * EN este programa se soluciona la pregunta 12 del examen 
  * @author eduar
  */
 public class Pregunta12 {
-    
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
+        System.out.print("Numero 1: ");
+        int a = leer.nextInt();
+        System.out.print("Numero 2: ");
+        int b = leer.nextInt();
+        System.out.print("Numero 3: ");
+        int c = leer.nextInt();
+        
+        int menor = numeroMenor(a,b,c);
+        System.out.println("El numero menor es: " + menor);
+        
+    }
+    public static int numeroMenor(int a, int b, int c){
+        if (a < b && a < c) return a;
+        else if (b < c) return b;
+        else return c;
+    }
 }
--- src/retos/reto1/Pregunta13.java
+++ src/retos/reto1/Pregunta13.java
@@ -1,14 +1,24 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package retos.reto1;
-
+import java.util.Scanner;
 /**
  *
  * @author eduar
  */
 public class Pregunta13 {
-    
+    public static void main(String[] args) {
+    Scanner leer = new Scanner(System.in);
+        int a;
+        System.out.print("Numero: ");
+        a = leer.nextInt();
+
+        serieDescendente(a);
+    }
+
+    public static void serieDescendente(int z) {
+        if (z >= 0) {
+            System.out.println(z);
+            serieDescendente(z - 1);
+        }
+    }
+
 }
